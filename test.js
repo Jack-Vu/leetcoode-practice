5cecd15f1461cb41bf3faa709051d811a709c995
Jack Vu <jacknvu98@gmail.com>
2024-09-09 19:51:40 -0400

    brain hurt

---
+++ test.js
@@ -0,0 +1,21 @@
+const array = [1, 3, 4, 5, 7, 10, 11];
+const target = 7;
+
+function isValid(nums, target) {
+  let l = 0;
+  let r = nums.length - 1;
+
+  while (l <= r) {
+    const m = Math.floor(l + (r - l) / 2);
+    if (nums[m] > target) {
+      r = m - 1;
+    } else if (nums[m] < target) {
+      l = m + 1;
+    } else {
+      return m;
+    }
+  }
+  return -1;
+}
+
+console.log(isValid(array, target));
