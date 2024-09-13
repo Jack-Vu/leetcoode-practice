ee6af0c31906b4c6ee38e1425a9f46c65affc936
Jack Vu <jacknvu98@gmail.com>
2024-09-13 19:28:44 -0400

    more stuff

--- test.js
+++ test.js
@@ -28,3 +28,35 @@ const char = ["a", "b"];
 
 console.log(word.includes(targetWord));
 console.log("a" in char);
+
+function maxSubArray(nums) {
+  let [runningSum, maxSum] = [nums[0], nums[0]];
+
+  for (let i = 1; i < nums.length; i++) {
+    const num = nums[i];
+    const sum = runningSum + num;
+
+    runningSum = Math.max(num, sum);
+    maxSum = Math.max(maxSum, runningSum);
+  }
+
+  return maxSum;
+}
+
+function test() {
+  const binaryRep = (0).toString(2);
+  let countOne = 0;
+  let countZero = 0;
+  for (let i = 0; i < binaryRep.length; i++) {
+    if (binaryRep === "0") {
+      countZero++;
+    }
+    if (binaryRep === "1") {
+      countOne++;
+    }
+  }
+  console.log("Count 0", countZero);
+  console.log("Count 1", countOne);
+}
+
+test();
